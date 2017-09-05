178b7723e9ce0ddb59d63a2db90f518b7e377c21
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-09-05 13:53:45 -0700

    Created category schema

--- lab-eddie/back/server.js
+++ lab-eddie/back/server.js
@@ -12,8 +12,11 @@ const PORT = process.env.PORT || 3000;
 const MONGODB_URI = 'mongodb://localhost/react';
 mongoose.connect(MONGODB_URI);
 
+const catRouter = require('./route/cat-route.js');
+
 app.use(cors);
 app.use(morgan('dev'));
+app.use(catRouter);
 
 app.listen(PORT, () => {
   debug('Server Active On Port: ', PORT);

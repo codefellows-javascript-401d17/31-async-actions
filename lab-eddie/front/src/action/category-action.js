dcf7489e2256ff27069373a34f83a1a27b6b1c44
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-09-05 17:05:32 -0700

    Wrote api calls in category action

--- lab-eddie/front/src/action/category-action.js
+++ lab-eddie/front/src/action/category-action.js
@@ -19,3 +19,37 @@ export const categoryDelete = (cat) => {
   type: 'CATEGORY_DELETE',
   payload: cat
 }
+
+export const fetchAllCats = () => (dispatch) => {
+  return superagent.get(`${__API_URL__}/api/category`)
+  .then(res => {
+    dispatch(categorySet(res.body));
+    return res;
+  })
+}
+
+export const createCatRequest = cat => dispatch => {
+  return superagent.post(`${__API_URL__}/api/category`)
+  .send(cat)
+  .then(res => {
+    dispatch(categoryCreate(res.body));
+    return res;
+  })
+}
+
+export const updateCatRequest = cat => dispatch => {
+  return superagent.put(`${__API_URL__}/api/category/${cat.id}`)
+  .send(cat)
+  .then(res => {
+    dispatch(categoryUpdate(res.body));
+    return res;
+  })
+}
+
+export const deleteCatRequest = cat => dispatch => {
+  return superagent.delete(`${__API_URL__}/api/category/${cat.id}`)
+  .then(res => {
+    dispatch(categoryUpdate(cat));
+    return res;
+  })
+}
